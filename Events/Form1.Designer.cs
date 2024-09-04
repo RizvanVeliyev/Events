1ec4123cecd1e5da95bd1bd9b3e7b3428f04de6e
RizvanVeliyev <veliyevrizvan250@gmail.com>
2024-09-04 10:36:05 +0400

    Add project files.

---
+++ Events/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Events
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.BtnSecondAdd = new System.Windows.Forms.Button();
+            this.BtnSecondPay = new System.Windows.Forms.Button();
+            this.BtnMainPay = new System.Windows.Forms.Button();
+            this.BtnMainAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(146, 407);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(117, 22);
+            this.textBox10.TabIndex = 11;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(12, 397);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(113, 32);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Mexaric";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(146, 355);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(117, 22);
+            this.textBox11.TabIndex = 9;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(12, 345);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(113, 32);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "Medaxil";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(146, 299);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(117, 22);
+            this.textBox12.TabIndex = 7;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(12, 289);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(102, 32);
+            this.label12.TabIndex = 6;
+            this.label12.Text = "Balans";
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(146, 155);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(117, 22);
+            this.textBox13.TabIndex = 17;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(12, 145);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(113, 32);
+            this.label13.TabIndex = 16;
+            this.label13.Text = "Mexaric";
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(146, 101);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(117, 22);
+            this.textBox14.TabIndex = 15;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(12, 91);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(113, 32);
+            this.label14.TabIndex = 14;
+            this.label14.Text = "Medaxil";
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(146, 46);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(117, 22);
+            this.textBox15.TabIndex = 13;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(12, 36);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(102, 32);
+            this.label15.TabIndex = 12;
+            this.label15.Text = "Balans";
+            // 
+            // BtnSecondAdd
+            // 
+            this.BtnSecondAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSecondAdd.Location = new System.Drawing.Point(361, 311);
+            this.BtnSecondAdd.Name = "BtnSecondAdd";
+            this.BtnSecondAdd.Size = new System.Drawing.Size(142, 42);
+            this.BtnSecondAdd.TabIndex = 19;
+            this.BtnSecondAdd.Text = "Medaxil";
+            this.BtnSecondAdd.UseVisualStyleBackColor = true;
+            this.BtnSecondAdd.Click += new System.EventHandler(this.BtnSecondAdd_Click);
+            // 
+            // BtnSecondPay
+            // 
+            this.BtnSecondPay.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSecondPay.Location = new System.Drawing.Point(361, 372);
+            this.BtnSecondPay.Name = "BtnSecondPay";
+            this.BtnSecondPay.Size = new System.Drawing.Size(142, 42);
+            this.BtnSecondPay.TabIndex = 20;
+            this.BtnSecondPay.Text = "Mexaric";
+            this.BtnSecondPay.UseVisualStyleBackColor = true;
+            this.BtnSecondPay.Click += new System.EventHandler(this.BtnSecondPay_Click);
+            // 
+            // BtnMainPay
+            // 
+            this.BtnMainPay.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnMainPay.Location = new System.Drawing.Point(361, 120);
+            this.BtnMainPay.Name = "BtnMainPay";
+            this.BtnMainPay.Size = new System.Drawing.Size(142, 42);
+            this.BtnMainPay.TabIndex = 22;
+            this.BtnMainPay.Text = "Mexaric";
+            this.BtnMainPay.UseVisualStyleBackColor = true;
+            this.BtnMainPay.Click += new System.EventHandler(this.BtnMainPay_Click);
+            // 
+            // BtnMainAdd
+            // 
+            this.BtnMainAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnMainAdd.Location = new System.Drawing.Point(361, 46);
+            this.BtnMainAdd.Name = "BtnMainAdd";
+            this.BtnMainAdd.Size = new System.Drawing.Size(142, 42);
+            this.BtnMainAdd.TabIndex = 23;
+            this.BtnMainAdd.Text = "Medaxil";
+            this.BtnMainAdd.UseVisualStyleBackColor = true;
+            this.BtnMainAdd.Click += new System.EventHandler(this.BtnMainAdd_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(611, 524);
+            this.Controls.Add(this.BtnMainAdd);
+            this.Controls.Add(this.BtnMainPay);
+            this.Controls.Add(this.BtnSecondPay);
+            this.Controls.Add(this.BtnSecondAdd);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.label12);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnMainAccAdd;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button BtnMainAccPay;
+        private System.Windows.Forms.Button BtnSecondAccAdd;
+        private System.Windows.Forms.Button BtnSecondAccPay;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button BtnSecondAdd;
+        private System.Windows.Forms.Button BtnSecondPay;
+        private System.Windows.Forms.Button BtnMainPay;
+        private System.Windows.Forms.Button BtnMainAdd;
+    }
+}
+
---
+++ Events/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Forms;
+
+namespace Events
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnMainAdd_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void BtnMainPay_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void BtnSecondAdd_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void BtnSecondPay_Click(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
